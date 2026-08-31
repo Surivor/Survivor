f08ebee863ba6e4f45755824051fcb2081734a5c
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-08-31 23:29:56 +0200

    feat(backend/auth/module): import jwt and configured it

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: 'DONT/ASK8FORTHEKEY!!!', // i will replace it in a secret in the .env
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
