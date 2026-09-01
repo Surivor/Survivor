e8566186f2f80885360fc1cc69b6960d9bbf3d93
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-01 23:33:44 +0200

    doc(backend): add swagger in pacakage json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Doc')
+    .setDescription('Documentation of api endpoint')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
   app.enableCors();
 
