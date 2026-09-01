519ce80c1b4102f1b604ad792cb1dc7aa9b76979
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-01 14:50:15 +0200

    feat(backend/transaction/module): add TypeOrm for the transaction

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PassportModule } from '@nestjs/passport';
+import { Transaction } from './entities/transaction.entity';
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([Transaction])
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
