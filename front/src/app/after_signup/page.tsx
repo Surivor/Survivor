b2e7d4e093c39ab59a81f479999fc757c5b68b83
titouan Menoret <titouan.menoret@epitech.eu>
2026-09-01 17:15:31 +0200

    feat(front): add a page after signup

---
+++ front/src/app/after_signup/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function WatingAccValidationPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-zinc-50 px-4">
+      <h1 className="flex flex-1 flex-col items-center justify-center gap-2 rounded-2xl bg-blue-600 px-6 py-10 text-center text-white">En attente de la validation du compte</h1>
+    </div>
+  );
+}
