a7a9718cf032a7002a9c0baf713fd30af4bd4e83
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-02 01:27:51 +0200

    doc(backend/partners): swagger doc describe dto

--- backend/src/partners/dto/create-partner.dto.ts
+++ backend/src/partners/dto/create-partner.dto.ts
@@ -2,17 +2,29 @@
 import { IsString, IsNotEmpty, ValidateNested} from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateUserDto } from "../../users/dto/create-user.dto";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePartnerDto {
+    @ApiProperty({
+        type: () => CreateUserDto,
+        description: 'User credentials and basic information linked to the new partner'
+    })
     @ValidateNested()
     @Type(() => CreateUserDto)
     userdto: CreateUserDto;
 
+    @ApiProperty({
+        example: 123456789,
+        description: 'The 9-digit SIREN number of the partner company'
+    })
     @IsNotEmpty({ message: "SIREN Required" })
     siren: number;
 
+    @ApiProperty({
+        example: 'Equestrian club and team building',
+        description: 'The registered social purpose or business sector of the partner'
+    })
     @IsString()
     @IsNotEmpty({ message: "Object Social Required" })
     objet_social: string;
 }
-
