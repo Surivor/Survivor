a3f61cf25039c13ea4b9805d7e24814252352523
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-09-07 23:13:16 +0200

    fix(notification): lock notification web socket with jwt

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotificationsGateway } from './notification.gateway';
 import { NotificationsService } from './notification.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET', 'DONT/ASK8FORTHEKEY!!!'),
+      }),
+    }),
+  ],
   providers: [NotificationsGateway, NotificationsService],
   exports: [NotificationsService],
 })
--- front/src/app/(secure)/main/page.tsx
+++ front/src/app/(secure)/main/page.tsx
@@ -71,13 +71,13 @@ export default function MainPage() {
     }, []);
 
     useEffect(() => {
-        const userId = getUserId();
-        if (!userId) return;
+        const token = getToken();
+        if (!token) return;
 
         const backendUrl = window.location.protocol + "//" + window.location.hostname + ":3000";
         const socket = io(backendUrl, {
             path: "/socket.io/",
-            query: { userId },
+            auth: { token },
             transports: ["websocket", "polling"],
         });
 
